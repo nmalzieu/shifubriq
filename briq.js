9a3c0a7063b3e0df708c22ef1b20bd7b6e1a5027
Christophe pasquier <christof.pasquier@gmail.com>
2017-07-10 15:42:14 +0200

    init-refactor

---
+++ briq.js
@@ -0,0 +1,8 @@
+const Briq = require('briq-api').Client;
+
+const briq = new Briq({
+  accessToken: 'IVGSeS7thwN1dWsJDhYTFXHjK6zLAKDGlVnqljLYe306DR45LqZ1hbs4uymKlcWt3MHuvmGQcUzGJQv6YLF3e862XMMg55CYKklXGHw06P3jBrHb0YkJtVg7XSfFgcEY',
+  baseURL: 'https://briq-staging.herokuapp.com/v0',
+});
+
+export default briq
